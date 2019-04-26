1997fe051946226e2d5098fb519b9403963dd0d3
ashishk011 <ashishk@qexon.com>
2019-04-26 19:43:16 +0530

    slider

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.button').click(function () {
+        $('.welcomeNote').addClass('d-none');
+        $('.siteHome').removeClass('d-none');
+    });
+
+    // $('.carousel').carousel({
+    //     interval: false,
+    //     wrap: false
+    // });
+ let ul = document.querySelector('.scrollArea')
+ targetLi = document.getElementById(liID);
+});
